16d2a2fe75c7b79cab5d621a7ab18f0a109cea04
sandesh31 <sandeshjain3112@gmail.com>
2017-09-09 23:07:10 +0530

    q2

---
+++ set1q2.cpp
@@ -0,0 +1,15 @@
+//2 - Check whether a number is even or odd. 
+#include <iostream>
+using namespace std;
+int main() {
+ int a;
+ cout<<"enter the value";
+ cin>>a;
+	if(a%2 == 0){
+		cout<<"even";
+	}
+	else{
+		cout<<"odd";
+	}
+	return 0;
+}
